c92dc500c2a15c85be9b62e6439c0b112e4fbdac
shunnnya <>
2024-10-04 14:39:04 +0500

    fc

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+    speed: 500,
+
+    spaceBetween: 100,
+   
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+  
+    
+  });
+  
+  
+  
